3a6282f57a30e2747517fbc43f58387110809030
Jenz <zazzyjz@gmail.com>
2018-04-13 10:35:22 -0500

    McCool

--- main.py
+++ main.py
@@ -1,29 +1,17 @@
 from flask import Flask, request, redirect, render_template
+import os
 
 app = Flask(__name__)
-app.config ['DEBUG']
-    
+app.config ['DEBUG']  
 
 @app.route('/')
 def index():
     return render_template('form.html')
 
-
 @app.route('/welcome', methods=['POST'])
-def welcome():
-
-  #  username = request.form['username']                             #Define placeholders
-  #  password = request.form['password']
-  #  verify_password = request.form['verify_password']
-  #  email = request.form['email']
-
-  #  username_error = request.form['username_error']
-  #  password_error = request.form['password_error']
-  #  verify_pass_error = request.form['verify_pass_error']
-  #  email_error = request.form['email_error']  
-
-    return render_template('Welcome.html')
-
+def valid():
+    username = request.form['username']                             
+    return render_template('Welcome.html', username = username)
 
 app.run()
     
---
+++ user-signup.py
@@ -0,0 +1,4 @@
+    username = request.form['username']                             #Define placeholders
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    email = request.form['email']
